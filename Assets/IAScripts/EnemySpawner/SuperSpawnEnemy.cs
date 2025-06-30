db2ab404f6e2338e2f9a2f87cb772ea443630ccb
Manuel Gandara <manugandara0@gmail.com>
2025-06-30 19:20:09 -0300

    Otra vez

--- Assets/IAScripts/EnemySpawner/SuperSpawnEnemy.cs
+++ Assets/IAScripts/EnemySpawner/SuperSpawnEnemy.cs
@@ -1,8 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SuperSpawnEnemy : MonoBehaviour
 {
+    [Header("Spawner Settings")]
+    public GameObject enemyPrefab;
+    public int cantidadEnemigos = 10;
+    public float delayEntreSpawns = 0.2f;
 
+    [Header("Puntos de Spawn")]
+    public Transform[] puntosDeSpawn = new Transform[4];
+
+    private List<GameObject> enemigosGenerados = new();
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemigosCoroutine());
+    }
+
+    private IEnumerable<(int index, int tagAleatorio)> GenerarEnemigos(int cantidad)
+    {
+        for (int i = 0; i < cantidad; i++)
+        {
+            int tagAleatorio = Random.Range(1, 3);
+            yield return (i, tagAleatorio);
+        }
+    }
+
+    private IEnumerator SpawnEnemigosCoroutine()
+    {
+        var enemigosDatos = GenerarEnemigos(cantidadEnemigos)
+            .Where(e => e.tagAleatorio == 1 || e.tagAleatorio == 2)
+            .OrderBy(e => e.tagAleatorio)                            
+            .ToList();                                              
+
+        var enemigosConSpawn = enemigosDatos.Select(e =>
+        {
+            int spawnIndex = Random.Range(0, puntosDeSpawn.Length);
+            return new
+            {
+                index = e.index,
+                tag = e.tagAleatorio,
+                spawnIndex = spawnIndex
+            };
+        }).OrderByDescending(e => e.spawnIndex)
+          .ToArray();                          
+
+        foreach (var e in enemigosConSpawn)
+        {
+            Transform punto = puntosDeSpawn[e.spawnIndex];
+
+            GameObject nuevoEnemigo = Instantiate(enemyPrefab, punto.position, Quaternion.identity);
+            enemigosGenerados.Add(nuevoEnemigo);
+
+            Enemy enemyScript = nuevoEnemigo.GetComponent<Enemy>();
+            if (enemyScript != null)
+            {
+                enemyScript.Tag = e.tag;
+            }
+
+            Debug.Log($"Enemy {e.index} tipo {e.tag} spawneado en punto {e.spawnIndex}");
+
+            yield return new WaitForSeconds(delayEntreSpawns);
+        }
+
+        Debug.Log("Todos los enemigos fueron spawneados.");
+    }
 }
+
